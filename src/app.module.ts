8ec7c70936175f5ecbc6b190b4c604b13ce7fa84
os-olga <olga.shcherbakova.cubex@gmail.com>
2023-04-24 16:05:50 +0300

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KozaksModule } from './kozaks/kozaks.module';
 
 @Module({
-  imports: [KozaksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: `127.0.0.1`,
+      port: 3306,
+      username: `root`,
+      password: `Root`,
+      database: `kozak`,
+      synchronize: true,
+      entities: [],
+    }),
+    KozaksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/kozaks/kozaks.module.ts
+++ src/kozaks/kozaks.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { KozaksService } from './services/kozaks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { KozaksController } from './controllers/kozaks.controller';
+import { KozakEntity } from './entities/kozak.entity';
+
+import { KozaksService } from './services/kozaks.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([KozakEntity])],
   controllers: [KozaksController],
   providers: [KozaksService],
 })
